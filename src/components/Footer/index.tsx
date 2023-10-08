cd972955c552e72838af659328463a50db042a56
Kris <chenxiluoo@gmail.com>
2023-10-08 13:04:51 +0800

    delete x.com

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -168,9 +168,9 @@ const Footer: React.FC = () => {
           <IconWechat2 fontSize={16} className="text-gray-500 hover:text-green-500 dark:text-gray-400 dark:hover:text-green-500" />
         </button> */}
 
-        // <a href="https://twitter.com/real_kai42" target="_blank" title="x" rel="noreferrer">
-        //   <IconTwitter fontSize={16} className="text-gray-500 hover:text-[#1DA1F2] dark:text-gray-400 dark:hover:text-[#1DA1F2]" />
-        // </a>
+{/*         <a href="https://twitter.com/real_kai42" target="_blank" title="x" rel="noreferrer">
+          <IconTwitter fontSize={16} className="text-gray-500 hover:text-[#1DA1F2] dark:text-gray-400 dark:hover:text-[#1DA1F2]" />
+        </a> */}
         <button
           className="cursor-pointer focus:outline-none "
           type="button"
